3f7cc816dd2cb00f7ae292bd40ef95432103fafa
huseyinbatta <huseyinbattal42@gmail.com>
2022-02-28 21:58:02 +0300

    NavLink Added.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { Link, Routes, Route } from "react-router-dom";
+import { Link, NavLink, Routes, Route } from "react-router-dom";
 import PersonDetail from './components/PersonDetail';
-const userIds=[1,3,5,7,123]
+const userIds = [1, 3, 5, 7, 123]
 
 function App() {
   return (
     <div className="App">
       <Link to="/Link1">First Link</Link> |{" "}
       <Link to="/Link2" style={{ display: "block", color: "yellowgreen" }}>Second Link</Link>
-      {userIds.map((id, index)=>(
-        <Link key={index} to={`/PersonDetail/${id}`}><br/>User {index}</Link>
+      <NavLink to="/ActiveLink">Active Link Test</NavLink>
+      {userIds.map((id, index) => (
+        <Link key={index} to={`/PersonDetail/${id}`}><br />User {index}</Link>
       ))}
       <Routes>
         <Route index element={<h1>Home Page</h1>} />
-        <Route path='/Link1' element={<h1>Link1 Page</h1>} />
+        <Route index element={<h1>Home Page</h1>} />
+        <Route path='/ActiveLink' style={({ isActive }) => {
+          return {margin:"1rem 0", color: isActive ? "red" : "gray"}
+        }} element={<h1>ActiveLink Page</h1>} />
         <Route path='/Link2' element={<h1>Link2 Page</h1>} />
-        <Route path='PersonDetail/:personId' element={<PersonDetail/>}></Route>
-        <Route path='*' element={<h1>There's nothing here!<br/><b>404 Not Found!</b></h1>}></Route>
+        <Route path='PersonDetail/:personId' element={<PersonDetail />}></Route>
+        <Route path='*' element={<h1>There's nothing here!<br /><b>404 Not Found!</b></h1>}></Route>
       </Routes>
     </div>
   );
